77925590ff2ed38d3eaf245119a56f5abd498c65
Jeferson Brito <jefersonbritodeoliveira@gmail.com>
2021-09-06 17:54:29 -0300

    qurato

--- calculadora/teste3.py
+++ calculadora/teste3.py
@@ -6,14 +6,12 @@ def calcular(*args):
 
 def diminuir (a,b):
     resultado= a-b
-    while resultado < 50:
-        print(resultado) 
     return resultado
 
 
 print(calcular(264,4,5))
 print(diminuir(5,20))
-
+print('terceiro bant')
 
 
 
